4f2a9288a52c3604a78449acf8100aa0dfa6d8f8
Vitaliy Ostapenko <soul33d@gmail.com>
2017-05-30 17:21:22 +0300

    Task3 fully completed.

--- src/com/homelearning/Task3.java
+++ src/com/homelearning/Task3.java
@@ -1,17 +1,31 @@
 package com.homelearning;
 
+import com.homelearning.task1.Currency;
 import com.homelearning.task1.Order;
 
 import java.util.Comparator;
-import java.util.Set;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 import static com.homelearning.Task2.*;
 
 public class Task3 {
     public static void main(String[] args) {
-        Set<Order> orderSet = new TreeSet<>(Comparator.comparingInt(Order::getPrice));
-        Task2.fillOrdersCollection(orderSet);
+        TreeSet<Order> orderSet = new TreeSet<>(Comparator.comparingInt(Order::getPrice)
+                .thenComparing(order -> -order.getCurrency().ordinal())
+                .thenComparing(order -> order.getUser().getFirstName())
+                .thenComparing(order -> order.getUser().getLastName()));
+        fillOrdersCollection(orderSet);
         printCollection(orderSet);
+
+        System.out.println("The most expensive order:\n" + orderSet.last() + "\n");
+
+        Iterator<Order> iterator = orderSet.iterator();
+        while (iterator.hasNext()){
+            Order order = iterator.next();
+            if (order.getCurrency() == Currency.USD) iterator.remove();
+        }
+
+        printCollection(Currency.USD + " removed from set", orderSet);
     }
 }
